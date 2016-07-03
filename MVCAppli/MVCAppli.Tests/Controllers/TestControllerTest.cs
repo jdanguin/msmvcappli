76e8c00bce2d64ebfbfffa8d752825578ecb2d32
juliend2016 <jdanguin@gmail.com>
2016-07-03 23:24:36 +0200

    Add test project MVCAppli

---
+++ MVCAppli/MVCAppli.Tests/Controllers/TestControllerTest.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using MVCAppli.Controllers;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using System.Web;
+using System.IO;
+
+namespace MVCAppli.Tests.Controllers
+{
+    [TestClass]
+    public class TestControllerTest
+    {
+        #region configuration
+        private const string WebApiUrl = "http://localhost/MVCAppli/api/test/";
+        private const string xml = "<TRANS><HPAY><lD>'103</lD><STATUS>3</STATUS><EXTRA><lS3DS>0</lS3DS><AUTH>03'1183</AUTH></EXTRA><lNT_MSG/><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>projectOl</MTOKEN></HPAY></TRANS>";
+        private const string badxmlformat = "<foo>hello</bar>";
+
+        /// <summary>
+        /// Set Up Context Controller 
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="httpMethod"></param>
+        /// <param name="controllerMethod"></param>
+        /// <returns></returns>
+        private TestController SetUpController(TestController controller, string httpMethod, string controllerMethod)
+        {
+            controller.Request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(WebApiUrl + controllerMethod),
+                Method = new HttpMethod(httpMethod)
+            };
+            controller.Configuration = new HttpConfiguration();
+            controller.Configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+
+            controller.RequestContext.RouteData = new HttpRouteData(
+                route: new HttpRoute(),
+                values: new HttpRouteValueDictionary { { "test", controllerMethod } });
+
+            return controller;
+        }
+        #endregion
+
+        #region Test XmlToJson
+        /// <summary>
+        /// Test Web Service Rest XmlToJson Api Ok
+        /// </summary>
+        [TestMethod]
+        public void XmlToJsonTestServiceOkPost()
+        {
+            // Arrange
+            var url = WebApiUrl + "xmltojson";
+            string response = null;
+
+            var URI = new Uri(url);
+            //serialize json object to string
+            string jsonText = JsonConvert.SerializeObject(xml);
+
+            // Act
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                response = client.UploadString(new Uri(url), "POST", jsonText);
+            }
+
+            var json = JsonConvert.DeserializeObject(response);
+
+            // Assert
+            Assert.IsNotNull(json);
+        }
+
+        /// <summary>
+        /// Test Controller method XmlToJson Ok
+        /// </summary>
+        [TestMethod]
+        public void XmlToJsonTestControllerOkPost()
+        {
+            // Arrange
+            TestController controller = new TestController();
+            controller = SetUpController(controller, "POST", "xmltojson");
+
+            // Act
+            var response = controller.PostXmlToJson(xml);
+            var json = JsonConvert.DeserializeObject(((ObjectContent)response.Content).Value as string);
+
+            // Assert
+            Assert.IsNotNull(json);
+        }
+
+        /// <summary>
+        /// Test Web Service Rest Api XmlToJson: case BadXmlFormat
+        /// </summary>
+        [TestMethod]
+        public void XmlToJsonTestServiceBadXmlFormatPost()
+        {
+            // Arrange
+            var url = WebApiUrl + "xmltojson";
+            string response = null;
+            //serialize json object to string
+            string jsonText = JsonConvert.SerializeObject(badxmlformat);
+
+            // Act
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                try
+                {
+                    response = client.UploadString(new Uri(url), "POST", jsonText);
+                }
+                catch (WebException exception)
+                {
+                    string responseText = null;
+
+                    if (exception.Response != null)
+                    {
+                        var responseStream = exception.Response.GetResponseStream();
+
+                        if (responseStream != null)
+                        {
+                            using (var reader = new StreamReader(responseStream))
+                            {
+                                responseText = reader.ReadToEnd();
+                                response = responseText;
+                            }
+                        }
+                    }
+                }
+            }
+            dynamic json = JObject.Parse(response);
+            // Assert
+            Assert.AreEqual("Bad Xml format", json.Message.Value);
+        }
+
+        /// <summary>
+        /// Test Controller method XmlToJson: case BadXmlFormat
+        /// </summary>
+        [TestMethod]
+        public void XmlToJsonTestControllerBadXmlFormatPost()
+        {
+            // Arrange
+            TestController controller = new TestController();
+            controller = SetUpController(controller, "POST", "xmltojson");
+
+            // Act
+            var response = controller.PostXmlToJson(badxmlformat);
+            var error = (HttpError)(((ObjectContent)response.Content).Value);
+
+            // Assert
+            Assert.AreEqual("Bad Xml format", error.Message);
+        }
+        #endregion
+
+        #region Test Fibonacci
+        /// <summary>
+        /// Test Web Service Rest Api Fibonacci: case 1 Ok
+        /// </summary>
+        [TestMethod]
+        public void FibonacciTestServiceCase1OkGet()
+        {
+            // Arrange
+            var url = WebApiUrl + "fibonacci";
+            string response = null;
+
+            // Act
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                response = client.DownloadString(new Uri(url + "/2"));
+            }
+
+            var resultNumber = int.Parse(response);
+
+            // Assert
+            Assert.AreEqual(1, resultNumber);
+        }
+
+        /// <summary>
+        /// Test Controller method Fibonacci: case 1 Ok
+        /// </summary>
+        [TestMethod]
+        public void FibonacciTestControllerCasOk1Get()
+        {
+            // Arrange
+            TestController controller = new TestController();
+            controller = SetUpController(controller, "GET", "fibonacci/2");
+
+            // Act
+            var response = controller.GetFibonacci(2);
+            var resultNumber = (int)((ObjectContent)response.Content).Value;
+
+            // Assert
+            Assert.IsTrue(((ObjectContent)response.Content).Value is int);
+            Assert.AreEqual(1, resultNumber);
+        }
+
+        /// <summary>
+        /// Test Web Service Rest Api Fibonacci: case 2 Ok
+        /// </summary>
+        [TestMethod]
+        public void FibonacciTestServiceCase2OkGet()
+        {
+            // Arrange
+            var url = WebApiUrl + "fibonacci";
+            string response = null;
+
+            // Act
+            using (var client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                response = client.DownloadString(new Uri(url + "/1000"));
+            }
+
+            var resultNumber = int.Parse(response);
+
+            // Assert
+            Assert.AreEqual(-1, resultNumber);
+        }
+
+        /// <summary>
+        /// Test Controller Fibonacci: case 2 Ok
+        /// </summary>
+        [TestMethod]
+        public void FibonacciTestControllerCaseOk2Get()
+        {
+            // Arrange
+            TestController controller = new TestController();
+            controller = SetUpController(controller, "GET", "fibonacci/1000");
+
+            // Act
+            var response = controller.GetFibonacci(1000);
+            var resultNumber = (int)((ObjectContent)response.Content).Value;
+
+            // Assert
+            Assert.AreEqual(-1, resultNumber);
+        }
+        #endregion
+    }
+}
---
+++ MVCAppli/MVCAppli/Controllers/TestController.cs
@@ -0,0 +1,97 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Xml;
+
+namespace MVCAppli.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        #region fibonacci
+        // GET: api/test/fibonacci/{number}
+        [Route("fibonacci/{number}")]
+        public HttpResponseMessage GetFibonacci(int number)
+        {
+            try
+            {
+                Log.Info(string.Format("Request GET api/test/fibonacci/{0}", number));
+                int resultNumber;
+                if (number >= 1 && number <= 100)
+                {
+                    resultNumber = GetFibonacciResult(number);
+                }
+                else
+                {
+                    resultNumber = -1;
+                }
+                Log.Info(string.Format("Response GET api/test/fibonacci/{0} - return {1}", number, resultNumber));
+                return Request.CreateResponse(HttpStatusCode.OK, resultNumber);
+            }
+            catch (Exception ex)
+            {
+                var message = String.Format("Error Fibonacci input");
+                var httpError = new HttpError(message);
+                Log.Error(message, ex);
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, httpError);
+            }
+        }
+
+        public int GetFibonacciResult(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        #endregion
+
+        #region XmlToJson
+        // POST: api/Test/xmltojson
+        [Route("xmltojson")]
+        public HttpResponseMessage PostXmlToJson([FromBody]string xml)
+        {
+            try
+            {
+                Log.Info(string.Format("Request POST api/test/xmltojson - Body: ", xml));
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+                if (jsonText == null)
+                {
+                    var message = String.Format("json not found");
+                    var httpError = new HttpError(message);
+                    Log.Info(string.Format("Response POST api/test/xmltojson - return {0}", message));
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, httpError);
+                }
+                Log.Info(string.Format("Response POST api/test/xmltojson - return {0}", jsonText));
+                return Request.CreateResponse(HttpStatusCode.OK, jsonText);
+            }
+            catch (Exception ex)
+            {
+                var message = String.Format("Bad Xml format");
+                var httpError = new HttpError(message);
+                httpError.ExceptionMessage = message;
+                Log.Error(message, ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, httpError);
+            }
+        }
+        #endregion
+    }
+}
