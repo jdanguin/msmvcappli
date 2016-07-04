bff8bcbf416bca4fe42a5c13d0a56ebfbd1832b2
juliend2016 <jdanguin@gmail.com>
2016-07-05 01:03:42 +0200

    Add Web Project Asmx

---
+++ WebAppliWSTEST/WebAppliWSTEST.Tests/WsTestUnitTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace WebAppliWSTEST.Tests
+{
+    [TestClass]
+    public class WsTestUnitTest
+    {
+        private const string xml = "<TRANS><HPAY><lD>'103</lD><STATUS>3</STATUS><EXTRA><lS3DS>0</lS3DS><AUTH>03'1183</AUTH></EXTRA><lNT_MSG/><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>projectOl</MTOKEN></HPAY></TRANS>";
+        private const string badxmlformat = "<foo>hello</bar>";
+
+        #region Fibonacci Test
+        [TestMethod]
+        public void TestWSFibonacciNumber0()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(0);
+
+            //Assert
+            Assert.AreEqual(-1, resultNumber);
+        }
+
+        [TestMethod]
+        public void TestWSFibonacciNumber1()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(1);
+
+            //Assert
+            Assert.AreEqual(1, resultNumber);
+        }
+
+        [TestMethod]
+        public void TestWSFibonacciNumber2()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(2);
+
+            //Assert
+            Assert.AreEqual(1, resultNumber);
+        }
+
+        [TestMethod]
+        public void TestWSFibonacciNumber75()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(75);
+
+            //Assert
+            Assert.AreEqual(2111485077978050, resultNumber);
+
+        }
+
+        [TestMethod]
+        public void TestWSFibonacciNumber100()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(100);
+
+            //Assert
+            Assert.AreEqual(3736710778780434371, resultNumber);
+
+        }
+
+        [TestMethod]
+        public void TestWSFibonacciNumber1000()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            long resultNumber = service.Fibonacci(1000);
+
+            //Assert
+            Assert.AreEqual(-1, resultNumber);
+        }
+        #endregion
+
+        #region XmlToJson Test
+        [TestMethod]
+        public void TestWSXmltojsonOk()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            string responseJson = service.XmlToJson(xml);
+            var json = JsonConvert.DeserializeObject(responseJson);
+
+            //Assert
+            Assert.IsNotNull(json);
+        }
+
+        [TestMethod]
+        public void TestWSXmltojsonBadXmlFormat()
+        {
+            // Arrange
+            var service = new WebAppliWSTESTReference.WebServiceTestSoapClient();
+
+            //Act
+            string responseJson = service.XmlToJson(badxmlformat);
+
+            //Assert
+            Assert.AreEqual("Bad Xml format", responseJson);
+        }
+        #endregion
+    }
+}
---
+++ WebAppliWSTEST/WebAppliWSTEST/WebServiceTest.asmx.cs
@@ -0,0 +1,120 @@
+﻿using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+using System.Xml;
+
+namespace WebAppliWSTEST
+{
+    /// <summary>
+    /// Summary description for WebServiceTest
+    /// </summary>
+    [WebService(Namespace = "http://localhost/WebAppliWSTEST/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebServiceTest : System.Web.Services.WebService
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        #region Fibonacci
+        // Fibonacci
+        /// <summary>
+        /// Use fibonacci formula with long number
+        /// with accept segmentation (of 1 to 100) else return -1
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <returns></returns>
+        [ScriptMethod(UseHttpGet = true,ResponseFormat = ResponseFormat.Json)]
+        [WebMethod]
+        public long Fibonacci(long number)
+        {
+            Context.Response.Clear();
+            Context.Response.ContentType = "application/json";
+
+            try
+            {
+                Log.Info(string.Format("Request Fibonacci - number: {0}", number));
+                long resultNumber;
+                if (number >= 1 && number <= 100)
+                {
+                    resultNumber = GetFibonacciResult(number);
+                }
+                else
+                {
+                    resultNumber = -1;
+                }
+                Log.Info(string.Format("Response Fibonacci - number {0} - return number {1}", number, resultNumber));
+                return resultNumber;
+            }
+            catch (Exception ex)
+            {
+                var message = String.Format("Error Fibonacci Method");
+                Log.Error(message, ex);
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// Fibonacci formula
+        /// </summary>
+        /// <param name="n">number</param>
+        /// <returns>number</returns>
+        public long GetFibonacciResult(long number)
+        {
+            long a = 0;
+            long b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (long i = 0; i < number; i++)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        #endregion
+
+        #region XMLToJson
+        //  Xmltojson
+        /// <summary>
+        /// Method which convert xml content to json
+        /// </summary>
+        /// <param name="xml">string</param>
+        /// <returns>string (json text)</returns>
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        [WebMethod]
+        public string XmlToJson(string xml)
+        {
+            try
+            {
+                Log.Info(string.Format("Request Xmltojson - xml: {0}", xml));
+                var doc = new XmlDocument();
+                doc.LoadXml(xml);
+                string jsonText = JsonConvert.SerializeXmlNode(doc);
+
+                if (jsonText == null)
+                {
+                    var message = String.Format("json not found");
+                    Log.Info(string.Format("Response Xmltojson - return json: {0}", message));
+                    return message;
+                }
+                Log.Info(string.Format("Response Xmltojson - return json {0}", jsonText));
+                return jsonText;
+            }
+            catch (Exception ex)
+            {
+                var message = String.Format("Bad Xml format");
+                Log.Error(message, ex);
+                return message;
+            }
+        }
+        #endregion
+    }
+}
